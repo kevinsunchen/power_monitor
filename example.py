381220d630bdc2e1e9a3ab1cb1e058e284da9995
Baolin Li <baolin.li1994@gmail.com>
2023-02-03 18:06:18 -0500

    monir update

--- example.py
+++ example.py
@@ -1,14 +1,29 @@
-from carbontracker.tracker import CarbonTracker
+from carbontracker.tracker import CarbonTracker, CarbonTrackerManual
 import time
 
-NUM_TRIAL = 3
-tracker = CarbonTracker(epochs=NUM_TRIAL, monitor_epochs=NUM_TRIAL, update_interval=1,
-            components='all', epochs_before_pred=NUM_TRIAL)
+tracker = CarbonTrackerManual(epochs=1, monitor_epochs=1, update_interval=1,
+        components='all', epochs_before_pred=1, verbose=2)
+tracker.tracker.pue_manual=1.5
+tracker.intensity_updater.ci_manual = 294
+# tracker = CarbonTracker(epochs=1, monitor_epochs=1, update_interval=1,
+#         components='all', epochs_before_pred=1, verbose=2)
 
-for i in range(NUM_TRIAL):
-    tracker.epoch_start()
+tracker.epoch_start()
 
-    time.sleep(10)
-    tracker.epoch_end()
+time.sleep(5)
+pass
 
-tracker.stop()
+tracker.epoch_end('test')
+
+pass
+# NUM_TRIAL = 3
+# tracker = CarbonTracker(epochs=NUM_TRIAL, monitor_epochs=NUM_TRIAL, update_interval=1,
+#             components='all', epochs_before_pred=NUM_TRIAL)
+
+# for i in range(NUM_TRIAL):
+#     tracker.epoch_start()
+
+#     time.sleep(10)
+#     tracker.epoch_end()
+
+# tracker.stop()
